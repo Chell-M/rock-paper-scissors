a0fe748aaaca815446f4cabd70b64edc1bc1aeed
Michele Mele <chelly@Micheles-MacBook-Pro.local>
2024-05-09 12:06:28 -0700

    rock paper scissors mock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+//requirements
+//user must select an option before starting game
+// start game button should be disabled until user selects option
+// clicking on start game runs cpu logic(generates random option)
+// display reset button thats dissabled until winner is announced
+//
+//add reset function
+//start game
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [userSelection, setUserSelection] = useState('')
+  const [cpuSelection, setCpuSelection] = useState('')
+  const [winner, setWinner] = useState()
+
+  const generateCpuSelection = () => {
+    const options = ['rock', 'paper', 'scissors']
+    const randomIndex = Math.floor(Math.random() * options.length);
+    setCpuSelection(options[randomIndex])
+  }
+
+  const updateUserSelection = (selection) => {
+    setUserSelection(selection)
+  }
+
+
+  const calculateWinner = () => {
+    //value is what loses
+    const lookup = {
+      rock: 'paper',
+      paper: 'scissors',
+      scissors: 'rock'
+    }
+    if (userSelection === cpuSelection) {
+      setWinner('Tie')
+      return;
+    }
+    if (lookup[userSelection] === cpuSelection) {
+      setWinner('Cpu')
+      return;
+    }
+    setWinner('User')
+  }
+
+  const resetGame = () => {
+    //reset stats
+  }
+
+  const startGame = () => {
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Start Game</button>
+      <p>selected option: {userSelection}</p>
+      <h3>user options</h3>
+      <button onClick={() => updateUserSelection('rock')}>Rock</button>
+      <button onClick={() => updateUserSelection('paper')}>Paper</button>
+      <button onClick={() => updateUserSelection('scissors')}>Scissors</button>
+      <p>selected CPU Option: {cpuSelection}</p>
+      <p>display winner</p>
+      <button onClick={() => calculateWinner()}>View Results</button>
+      <h1>{winner}</h1>
     </div>
   );
 }
---
+++ src/components/game.js
@@ -0,0 +1,6 @@
+function Game() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
